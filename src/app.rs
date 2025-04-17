e9313c671a67bca9603421c185b294af766565f8
Asurakun1 <udon3699@gmail.com>
2025-04-16 21:21:19 -0700

    re import

---
+++ src/app.rs
@@ -0,0 +1,171 @@
+use std::{error::Error, rc::Rc};
+
+use ratatui::{
+    crossterm::event::{self, Event, KeyCode, KeyEventKind},
+    layout::{Constraint, Direction, Layout, Rect},
+    style::{Color, Style, Styled},
+    widgets::{Block, Paragraph, RatatuiLogo, Wrap},
+    DefaultTerminal, Frame,
+};
+
+#[derive(PartialEq)]
+enum Menu {
+    Menu,
+    Exit,
+}
+
+pub struct App {
+    app_state: Menu,
+    terminal: DefaultTerminal,
+    cycle: u8,
+}
+
+impl App {
+    pub fn new(terminal: DefaultTerminal) -> Self {
+        Self {
+            cycle: 0,
+            terminal,
+            app_state: Menu::Menu,
+        }
+    }
+
+    pub fn run(&mut self) -> Result<(), Box<dyn Error>> {
+        while self.app_state != Menu::Exit {
+            self.terminal.draw(|frame| {
+                render_callback(frame, self.cycle);
+            })?;
+            handle_events(self)?;
+        }
+        Ok(())
+    }
+}
+
+//Draws the main screen
+fn render_callback(frame: &mut Frame, cycle: u8) {
+    //outer layer main screen block
+    let layer_0 = chunks(frame.area());
+    let (menu_block, divider) = main_screen_block(Rc::clone(&layer_0));
+    frame.render_widget(&menu_block, layer_0[0]);
+
+    //list of modules (implemented or not)
+    menu_selection(frame, &menu_block, &divider, &cycle);
+    menu_right_chunk(frame, Rc::clone(&divider), &menu_block);
+}
+
+fn main_screen_block<'a>(layer_0: Rc<[Rect]>) -> (Block<'a>, Rc<[Rect]>) {
+    let menu_block = Block::bordered()
+        .style(Style::new().fg(Color::Green))
+        .title("Main Menu");
+
+    let divider = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Fill(50), Constraint::Fill(50)])
+        .split(layer_0[0]);
+    (menu_block, divider)
+}
+
+fn menu_right_chunk(frame: &mut Frame, divider: Rc<[Rect]>, menu: &Block) {
+    let inner_right_chunk = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(
+            [
+                Constraint::Fill(10),
+                Constraint::Fill(40),
+                Constraint::Fill(10),
+            ]
+            .as_ref(),
+        )
+        .split(menu.inner(divider[1]));
+
+    let ctrl = Paragraph::new("Commands: \u{2191} / \u{2193} / Enter（選択）/ Q （終了）")
+        .block(Block::bordered());
+    let logo = RatatuiLogo::default().size(ratatui::widgets::RatatuiLogoSize::Small);
+    let block = Block::bordered();
+
+    frame.render_widget(ctrl, inner_right_chunk[0]);
+    frame.render_widget(logo, inner_right_chunk[1]);
+    frame.render_widget(block, inner_right_chunk[2]);
+}
+
+fn menu_selection(frame: &mut Frame, menu: &Block, divider: &Rc<[Rect]>, cycle: &u8) {
+    let inner_left_chunk = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(
+            [
+                Constraint::Fill(10),
+                Constraint::Fill(10),
+                Constraint::Fill(10),
+                Constraint::Fill(10),
+            ]
+            .as_ref(),
+        )
+        .split(menu.inner(divider[0]));
+
+    let mut item_1 = Paragraph::new("Dictionary（辞書）")
+        .block(Block::bordered())
+        .wrap(Wrap { trim: false });
+    let mut item_2 = Paragraph::new("すべてのリストが選択可能です。")
+        .block(Block::bordered())
+        .wrap(Wrap { trim: true });
+    let mut item_3 = Paragraph::new("でも、選択だとしても。今からは何も動いていない。")
+        .block(Block::bordered())
+        .wrap(Wrap { trim: true });
+    let mut item_4 = Paragraph::new(
+        "中の物にはまだ何もない。でも多分一つの選択しは自分の辞書を作るためかもしれない。",
+    )
+    .block(Block::bordered())
+    .wrap(Wrap { trim: true });
+
+    match cycle {
+        0 => {
+            item_1 = item_1.set_style(Style::new().bg(Color::Green).fg(Color::Black));
+        }
+        1 => {
+            item_2 = item_2.set_style(Style::new().bg(Color::Green).fg(Color::Black));
+        }
+        2 => item_3 = item_3.set_style(Style::new().bg(Color::Green).fg(Color::Black)),
+        3 => item_4 = item_4.set_style(Style::new().bg(Color::Green).fg(Color::Black)),
+        _ => {}
+    }
+
+    frame.render_widget(item_1, inner_left_chunk[0]);
+    frame.render_widget(item_2, inner_left_chunk[1]);
+    frame.render_widget(item_3, inner_left_chunk[2]);
+    frame.render_widget(item_4, inner_left_chunk[3]);
+}
+
+fn chunks(chunk: Rect) -> Rc<[Rect]> {
+    Layout::default()
+        .constraints([Constraint::Fill(10)])
+        .split(chunk)
+}
+
+fn handle_events(app: &mut App) -> Result<(), Box<dyn Error>> {
+    match event::read() {
+        Ok(Event::Key(key)) if key.kind == KeyEventKind::Press => match key.code {
+            KeyCode::Char('q') => app.app_state = Menu::Exit,
+            KeyCode::Down => {
+                if app.cycle < 3 {
+                    app.cycle += 1;
+                } else {
+                    app.cycle = 3;
+                }
+            }
+            KeyCode::Up => {
+                if app.cycle > 0 {
+                    app.cycle -= 1;
+                } else {
+                    app.cycle = 0;
+                }
+            }
+            KeyCode::Enter => match app.cycle {
+                0 => {}
+                _ => (),
+            },
+            _ => (),
+        },
+        _ => (),
+    }
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use my_terminal::app::App;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let terminal = ratatui::init();
+    let mut app = App::new(terminal);
+    app.run()?;
+
+    ratatui::restore();
+
+    Ok(())
 }
